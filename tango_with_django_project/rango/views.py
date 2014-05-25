a94bfdc57ac46f9ff83df6500016ab53ac390d2e
brownmagik352 <sumanicus@gmail.com>
2014-05-25 11:04:39 -0400

    up to ch. 12 exercises

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from rango.bing_search import run_query
 
 def encode_url(str):
 	return str.replace(' ', '_')
@@ -193,6 +194,19 @@ def user_logout(request):
 def restricted(request):
 	context = RequestContext(request)
 	return render_to_response('rango/restricted.html', {}, context)
+
+def search(request):
+    context = RequestContext(request)
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render_to_response('rango/search.html', {'result_list': result_list}, context)
 
 
 
