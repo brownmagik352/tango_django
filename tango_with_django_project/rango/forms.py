d733765800966d74a3687fa32ef8eb5e648c0a2b
brownmagik352 <sumanicus@gmail.com>
2014-05-25 10:21:24 -0400

    completed ch. 11

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -29,17 +29,19 @@ class PageForm(forms.ModelForm):
 		model = Page
 		fields = ('title', 'url', 'views')
 
-#default form, built-in
 class UserForm(forms.ModelForm):
-	# overrides default password which does not hide password
-	password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(help_text="Please enter a username.")
+    email = forms.CharField(help_text="Please enter your email.")
+    password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
 
-	class Meta:
-		model = User
-		fields = ('username', 'email', 'password')
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
 
-#separate form for extension of User model (UserProfile)
 class UserProfileForm(forms.ModelForm):
-	class Meta:
-		model = UserProfile
-		fields = ('website', 'picture')
+    website = forms.URLField(help_text="Please enter your website.", required=False)
+    picture = forms.ImageField(help_text="Select a profile image to upload.", required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = ['website', 'picture']
